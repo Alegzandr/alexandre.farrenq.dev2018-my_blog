20ec3e08762249ea870c6e8bd34d18b9aaaf76f9
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-02-15 16:42:41 +0100

    TODO: Send register datas to database

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -2,6 +2,13 @@ $(function () {
     $('form[name="signup"]').submit(function () {
         $('.errors').remove();
 
+        var username = $(this).find(':text[name="username"]');
+        var firstName = $(this).find(':text[name="first-name"]');
+        var lastName = $(this).find(':text[name="last-name"]');
+        var mail = $(this).find('input[name="mail"]');
+        var password = $(this).find(':password[name="password"]');
+        var password2 = $(this).find(':password[name="password2"]');
+
         $.post($(this).attr('action'),
             $(this).serialize(),
             function (data) {
@@ -9,7 +16,24 @@ $(function () {
                     // If form ok
                 }
                 else {
-                    // If not
+                    if (data.username) {
+                        username.after(data.username);
+                    }
+                    if (data.firstName) {
+                        firstName.after(data.firstName);
+                    }
+                    if (data.lastName) {
+                        lastName.after(data.lastName);
+                    }
+                    if (data.mail) {
+                        mail.after(data.mail);
+                    }
+                    if (data.password) {
+                        password.after(data.password);
+                    }
+                    if (data.password2) {
+                        password2.after(data.password2);
+                    }
                 }
             }, 'json');
 
