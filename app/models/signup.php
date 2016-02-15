f2cd4a596bdf476d3aabd8e2324a27b27c0b5337
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-02-15 16:28:33 +0100

    Register PHP script ok

--- app/models/signup.php
+++ app/models/signup.php
@@ -16,7 +16,62 @@ if (!empty($_POST)) {
     $password = htmlentities($_POST['password']);
     $password2 = htmlentities($_POST['password2']);
 
-    // Checking conditions ...
+    if (!isset($username) || empty($username)) {
+        $errors['username'] = '<span class="errors">Non saisi</span>';
+        $valid = false;
+    } elseif (strlen($username) < 6) {
+        $errors['username'] = '<span class="errors">6 caractères min</span>';
+        $valid = false;
+    } elseif (strlen($username) > 24) {
+        $errors['username'] = '<span class="errors">24 caractères max</span>';
+        $valid = false;
+    }
+
+    if (!isset($first_name) || empty($first_name)) {
+        $errors['firstName'] = '<span class="errors">Non saisi</span>';
+        $valid = false;
+    } elseif (strlen($first_name) < 2) {
+        $errors['firstName'] = '<span class="errors">2 caractères min</span>';
+        $valid = false;
+    } elseif (strlen($first_name) > 32) {
+        $errors['firstName'] = '<span class="errors">32 caractères max</span>';
+        $valid = false;
+    }
+
+    if (!isset($last_name) || empty($last_name)) {
+        $errors['lastName'] = '<span class="errors">Non saisi</span>';
+        $valid = false;
+    } elseif (strlen($last_name) < 2) {
+        $errors['lastName'] = '<span class="errors">2 caractères min</span>';
+        $valid = false;
+    } elseif (strlen($last_name) > 32) {
+        $errors['lastName'] = '<span class="errors">32 caractères max</span>';
+        $valid = false;
+    }
+
+    if (!isset($mail) || empty($mail)) {
+        $errors['mail'] = '<span class="errors">Non saisi</span>';
+        $valid = false;
+    } elseif (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        $errors['mail'] = '<span class="errors">Format incorrect</span>';
+        $valid = false;
+    }
+
+    if (!isset($password) || empty($password)) {
+        $errors['password'] = '<span class="errors">Non saisi</span>';
+        $valid = false;
+    } elseif (strlen($password) < 8) {
+        $errors['password'] = '<span class="errors">8 caractères min</span>';
+        $valid = false;
+    }
+
+    if (!isset($password2) || empty($password2)) {
+        $errors['password2'] = '<span class="errors">Non saisi</span>';
+        $valid = false;
+    } elseif ($password2 !== $password) {
+        $errors['password2'] = '<span class="errors">Non identiques</span>';
+        $valid = false;
+    }
 
     if ($valid) {
         $errors['valid'] = $valid;
